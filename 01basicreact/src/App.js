2599b848441b49559111fa7aff4287fad89ed0b6
Sunny Jaiswal <jaiswalsunny055@gmail.com>
2023-11-12 02:19:10 +0530

    lec3 complete

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,12 @@
-
+import Chai from "./chai.js";
 
 function App() {
   return (
-   <h1>Chai aur react</h1>
+  //  <h1>Chai aur react</h1>
+  <>
+  <Chai/>
+  <h3>my name is sunny</h3>
+  </>
   );
 }
 
--- 02viteReact/src/App.jsx
+++ 02viteReact/src/App.jsx
@@ -1,8 +1,11 @@
-
+import Chai from "./chai"
 function App() {
   return (
+    <>
     <h1>Chai aur code with vite</h1>
-   
+    <Chai/>
+    </>
+    
   )
 }
 
